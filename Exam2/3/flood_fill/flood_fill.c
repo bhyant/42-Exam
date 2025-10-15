960143fcb038d01f1eb7714be3ee38dacaebf2e5
tbhuiyan <tbhuiyan@student.42.fr>
2025-10-15 14:42:58 +0200

    modification d'un commentaire sur flood_fill

--- Exam2/3/flood_fill/flood_fill.c
+++ Exam2/3/flood_fill/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: tbhuiyan <tbhuiyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 19:54:44 by tbhuiyan          #+#    #+#             */
-/*   Updated: 2025/09/24 20:03:17 by tbhuiyan         ###   ########.fr       */
+/*   Updated: 2025/10/15 14:42:10 by tbhuiyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void fill(char **tab, t_point size, char target, int pos_y, int pos_x)
 {
 	if (pos_y < 0 || pos_x < 0 || pos_y >= size.y || pos_x >= size.x) // on check si les valeurs entree sont coherente
 		return;
-	if (tab[pos_y][pos_x] == 'F' || tab[pos_y][pos_x] != target) // on check si la case est deja visite ou que la case de depart n'est pas la target
+	if (tab[pos_y][pos_x] == 'F' || tab[pos_y][pos_x] != target) // on check si la case est deja visite ou que la case n'est pas une case explorable
 		return;
 	tab[pos_y][pos_x] = 'F'; // on mets le F une fois que la case est visite
 	fill(tab, size, target, pos_y -1, pos_x); // recursive dans tout les sens 
