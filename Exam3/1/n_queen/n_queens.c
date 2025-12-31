540fea3036ce83862cf1d22e49830eebf60ef655
Taj Bhuiyan <tbhuiyan@f1br7s1.clusters.42paris.fr>
2025-12-31 14:18:18 +0100

    rectification n_queens

--- Exam3/1/n_queen/n_queens.c
+++ Exam3/1/n_queen/n_queens.c
@@ -6,64 +6,67 @@
 /*   By: tbhuiyan <tbhuiyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 23:19:19 by tbhuiyan          #+#    #+#             */
-/*   Updated: 2025/12/29 23:27:44 by tbhuiyan         ###   ########.fr       */
+/*   Updated: 2025/12/31 09:37:20 by tbhuiyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 
-int	is_safe(int	*board, int col, int row)
+int is_safe(int *board, int col, int row)
 {
-	int	i = 0;
+    int i;
 
-	while (i < col)
+	i = 0;
+    while(i < col)
 	{
-		if (board[i] == row || row - board[i] == col - i || board[i] - row == col - i)
-			return (0);
-		i++;
-	}
-	return (1);
+        if(board[i] == row || (row - board[i] == col - i) || (board[i] - row == col - i))
+            return (0);
+        i++;
+    }
+    return 1;
 }
 
-void	solve(int *board, int col, int n)
+void solve(int *board, int n, int col)
 {
-	int	row = 0;
-	int	i = 0;
+	int i;
+	int row;
 
-	if (col == n)
+    if (col == n)
 	{
-		while (i < n)
+        i = 0;
+        while (i < n)
 		{
-			if (i > 0)
+            if (i > 0)
 				printf(" ");
-			printf("%d", board[i]);
-			i++;
-		}
-		printf("\n");
-	}
-	while (row < n)
+            printf("%d", board[i]);
+            i++;
+        }
+        printf("\n");
+        return;
+    }
+    row = 0;
+    while (row < n)
 	{
-		if (is_safe(board, col, row))
+        if (is_safe(board, col, row))
 		{
-			board[col] = row;
-			solve(board, col + 1, n);
-		}
-		row++;
-	}
+            board[col] = row;
+            solve(board, n, col + 1);
+        }
+        row++;
+    }
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
 	if (ac == 2)
 	{
 		int	n = atoi(av[1]);
-		int	board[20];
+		int	board[20]
 
-		if (n < 4)
+		if (n < 1)
 			return 1;
-		solve(board, 0, n);
+		solve(board, n, 0);
 	}
 	else
 		return 1;
