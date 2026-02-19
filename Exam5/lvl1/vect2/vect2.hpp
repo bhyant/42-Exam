b0655a553d6385dc38c2c240b032ce78910e7a6f
tbhuiyan <tbhuiyan@student.42.fr>
2026-02-19 21:16:12 +0100

    Ajout vect2.hpp, .cpp en cours.

---
+++ Exam5/lvl1/vect2/vect2.hpp
@@ -0,0 +1,45 @@
+#ifndef VECT2_HPP
+#define VECT2_HPP
+
+#include <iostream>
+
+class vect2
+{
+	public:
+		int x;
+		int y;
+	private:
+		vect2();
+		vect2(int a, int b);
+		vect2(const vect2& src);
+		vect2& operator=(const vect2& src);
+		~vect2();
+
+		int operator[](int index) const;
+		int& operator[](int indec);
+
+		vect2	operator-() const;
+		vect2	operator*(int num) const;
+
+		vect2& operator*=(int num);
+
+		vect2& operator+=(const vect2& src);
+		vect2& operator-=(const vect2& src);
+		vect2& operator*=(const vect2& src);
+
+		vect2 operator+(const vect2& src) const;
+		vect2 operator-(const vect2& src) const;
+		vect2 operator*(const vect2& src) const;
+
+		vect2 operator++(int);
+		vect2 operator--(int);
+		vect2& operator++();
+		vect2& operator--();
+
+		bool operator==(const vect2& src) const;
+		bool operator!=(const vect2& src) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const vect2& src);
+
+#endif
